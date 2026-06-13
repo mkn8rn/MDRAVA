773ba5cdfd508fbbcd85001d49102898444d089e
Markinatorina <markinatorina@hotmail.com>
2026-06-13 07:58:13 +0200

    Split configuration route generated responses

---
+++ MDRAVA.API/Controllers/ProxyConfigurationRouteGeneratedResponse.cs
@@ -0,0 +1,96 @@
+using BusinessRuntimeCanonicalHostPolicy = MDRAVA.BLL.Configuration.RuntimeCanonicalHostPolicy;
+using BusinessRuntimeHttpsRedirectPolicy = MDRAVA.BLL.Configuration.RuntimeHttpsRedirectPolicy;
+using BusinessRuntimeMaintenancePolicy = MDRAVA.BLL.Configuration.RuntimeMaintenancePolicy;
+using BusinessRuntimePathRewritePolicy = MDRAVA.BLL.Configuration.RuntimePathRewritePolicy;
+using BusinessRuntimeRedirectPolicy = MDRAVA.BLL.Configuration.RuntimeRedirectPolicy;
+using BusinessRuntimeStaticResponse = MDRAVA.BLL.Configuration.RuntimeStaticResponse;
+
+namespace MDRAVA.API.Controllers;
+
+public sealed record RuntimeHttpsRedirectResponse(
+    bool Enabled,
+    int StatusCode,
+    int? HttpsPort)
+{
+    public static RuntimeHttpsRedirectResponse FromPolicy(BusinessRuntimeHttpsRedirectPolicy policy)
+    {
+        ArgumentNullException.ThrowIfNull(policy);
+
+        return new RuntimeHttpsRedirectResponse(policy.Enabled, policy.StatusCode, policy.HttpsPort);
+    }
+}
+
+public sealed record RuntimeCanonicalHostResponse(
+    bool Enabled,
+    string TargetHost,
+    int StatusCode)
+{
+    public static RuntimeCanonicalHostResponse FromPolicy(BusinessRuntimeCanonicalHostPolicy policy)
+    {
+        ArgumentNullException.ThrowIfNull(policy);
+
+        return new RuntimeCanonicalHostResponse(policy.Enabled, policy.TargetHost, policy.StatusCode);
+    }
+}
+
+public sealed record RuntimePathRewriteResponse(
+    string StripPrefix,
+    string ReplacePrefix,
+    string Replacement)
+{
+    public static RuntimePathRewriteResponse FromPolicy(BusinessRuntimePathRewritePolicy policy)
+    {
+        ArgumentNullException.ThrowIfNull(policy);
+
+        return new RuntimePathRewriteResponse(policy.StripPrefix, policy.ReplacePrefix, policy.Replacement);
+    }
+}
+
+public sealed record RuntimeRedirectResponse(
+    int StatusCode,
+    string TargetUrl,
+    string TargetPath,
+    bool PreserveQuery)
+{
+    public static RuntimeRedirectResponse FromPolicy(BusinessRuntimeRedirectPolicy policy)
+    {
+        ArgumentNullException.ThrowIfNull(policy);
+
+        return new RuntimeRedirectResponse(
+            policy.StatusCode,
+            policy.TargetUrl,
+            policy.TargetPath,
+            policy.PreserveQuery);
+    }
+}
+
+public sealed record RuntimeStaticResponseResponse(
+    int StatusCode,
+    string ContentType,
+    string Body)
+{
+    public static RuntimeStaticResponseResponse FromResponse(BusinessRuntimeStaticResponse response)
+    {
+        ArgumentNullException.ThrowIfNull(response);
+
+        return new RuntimeStaticResponseResponse(response.StatusCode, response.ContentType, response.Body);
+    }
+}
+
+public sealed record RuntimeMaintenanceResponse(
+    bool Enabled,
+    int? RetryAfterSeconds,
+    string ContentType,
+    string Body)
+{
+    public static RuntimeMaintenanceResponse FromPolicy(BusinessRuntimeMaintenancePolicy policy)
+    {
+        ArgumentNullException.ThrowIfNull(policy);
+
+        return new RuntimeMaintenanceResponse(
+            policy.Enabled,
+            policy.RetryAfterSeconds,
+            policy.ContentType,
+            policy.Body);
+    }
+}
--- MDRAVA.API/Controllers/ProxyConfigurationRouteResponse.cs
+++ MDRAVA.API/Controllers/ProxyConfigurationRouteResponse.cs
@@ -1,15 +1,9 @@
 using BusinessRuntimeCachePolicy = MDRAVA.BLL.Configuration.RuntimeCachePolicy;
-using BusinessRuntimeCanonicalHostPolicy = MDRAVA.BLL.Configuration.RuntimeCanonicalHostPolicy;
 using BusinessRuntimeHealthCheckOptions = MDRAVA.BLL.Configuration.RuntimeHealthCheckOptions;
-using BusinessRuntimeHttpsRedirectPolicy = MDRAVA.BLL.Configuration.RuntimeHttpsRedirectPolicy;
-using BusinessRuntimeMaintenancePolicy = MDRAVA.BLL.Configuration.RuntimeMaintenancePolicy;
-using BusinessRuntimePathRewritePolicy = MDRAVA.BLL.Configuration.RuntimePathRewritePolicy;
-using BusinessRuntimeRedirectPolicy = MDRAVA.BLL.Configuration.RuntimeRedirectPolicy;
 using BusinessRuntimeRetryPolicy = MDRAVA.BLL.Configuration.RuntimeRetryPolicy;
 using BusinessRuntimeRoute = MDRAVA.BLL.Configuration.RuntimeRoute;
 using BusinessRuntimeRouteAction = MDRAVA.BLL.Configuration.RuntimeRouteAction;
 using BusinessRuntimeRouteResolvedOptions = MDRAVA.BLL.Configuration.RuntimeRouteResolvedOptions;
-using BusinessRuntimeStaticResponse = MDRAVA.BLL.Configuration.RuntimeStaticResponse;
 
 namespace MDRAVA.API.Controllers;
 
@@ -89,94 +83,6 @@ public sealed record RuntimeHealthCheckResponse(
             options.Timeout,
             options.HealthyThreshold,
             options.UnhealthyThreshold);
-    }
-}
-
-public sealed record RuntimeHttpsRedirectResponse(
-    bool Enabled,
-    int StatusCode,
-    int? HttpsPort)
-{
-    public static RuntimeHttpsRedirectResponse FromPolicy(BusinessRuntimeHttpsRedirectPolicy policy)
-    {
-        ArgumentNullException.ThrowIfNull(policy);
-
-        return new RuntimeHttpsRedirectResponse(policy.Enabled, policy.StatusCode, policy.HttpsPort);
-    }
-}
-
-public sealed record RuntimeCanonicalHostResponse(
-    bool Enabled,
-    string TargetHost,
-    int StatusCode)
-{
-    public static RuntimeCanonicalHostResponse FromPolicy(BusinessRuntimeCanonicalHostPolicy policy)
-    {
-        ArgumentNullException.ThrowIfNull(policy);
-
-        return new RuntimeCanonicalHostResponse(policy.Enabled, policy.TargetHost, policy.StatusCode);
-    }
-}
-
-public sealed record RuntimePathRewriteResponse(
-    string StripPrefix,
-    string ReplacePrefix,
-    string Replacement)
-{
-    public static RuntimePathRewriteResponse FromPolicy(BusinessRuntimePathRewritePolicy policy)
-    {
-        ArgumentNullException.ThrowIfNull(policy);
-
-        return new RuntimePathRewriteResponse(policy.StripPrefix, policy.ReplacePrefix, policy.Replacement);
-    }
-}
-
-public sealed record RuntimeRedirectResponse(
-    int StatusCode,
-    string TargetUrl,
-    string TargetPath,
-    bool PreserveQuery)
-{
-    public static RuntimeRedirectResponse FromPolicy(BusinessRuntimeRedirectPolicy policy)
-    {
-        ArgumentNullException.ThrowIfNull(policy);
-
-        return new RuntimeRedirectResponse(
-            policy.StatusCode,
-            policy.TargetUrl,
-            policy.TargetPath,
-            policy.PreserveQuery);
-    }
-}
-
-public sealed record RuntimeStaticResponseResponse(
-    int StatusCode,
-    string ContentType,
-    string Body)
-{
-    public static RuntimeStaticResponseResponse FromResponse(BusinessRuntimeStaticResponse response)
-    {
-        ArgumentNullException.ThrowIfNull(response);
-
-        return new RuntimeStaticResponseResponse(response.StatusCode, response.ContentType, response.Body);
-    }
-}
-
-public sealed record RuntimeMaintenanceResponse(
-    bool Enabled,
-    int? RetryAfterSeconds,
-    string ContentType,
-    string Body)
-{
-    public static RuntimeMaintenanceResponse FromPolicy(BusinessRuntimeMaintenancePolicy policy)
-    {
-        ArgumentNullException.ThrowIfNull(policy);
-
-        return new RuntimeMaintenanceResponse(
-            policy.Enabled,
-            policy.RetryAfterSeconds,
-            policy.ContentType,
-            policy.Body);
     }
 }
 
