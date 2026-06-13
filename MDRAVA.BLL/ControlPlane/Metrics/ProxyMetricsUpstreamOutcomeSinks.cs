5ffb4aecfa96082b9f41268ce4c472d6d521524e
Markinatorina <markinatorina@hotmail.com>
2026-06-13 14:24:42 +0200

    Split proxy metrics request and upstream sinks

--- MDRAVA.BLL/ControlPlane/Metrics/ProxyMetricsUpstreamOutcomeSinks.cs
+++ MDRAVA.BLL/ControlPlane/Metrics/ProxyMetricsUpstreamOutcomeSinks.cs
@@ -2,22 +2,6 @@ namespace MDRAVA.BLL.ControlPlane.Metrics;
 
 public sealed partial class ProxyMetrics
 {
-    public void RequestReceived()
-    {
-        Interlocked.Increment(ref _totalRequests);
-    }
-
-    public void RequestCompleted(string? site, string? route, string? action, int? statusCode)
-    {
-        var key = new RequestSeriesKey(
-            ProxyMetricLabelPolicy.NormalizeValue(site),
-            ProxyMetricLabelPolicy.NormalizeValue(route),
-            ProxyMetricLabelPolicy.NormalizeValue(action),
-            ProxyMetricLabelPolicy.StatusClass(statusCode));
-        var counter = _requestsByRoute.GetOrAdd(key, static _ => new RequestSeriesCounter());
-        Interlocked.Increment(ref counter.Count);
-    }
-
     public void UpstreamSucceeded()
     {
         Interlocked.Increment(ref _upstreamSuccesses);
