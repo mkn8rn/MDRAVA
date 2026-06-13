29cad59bc34a77d4d8b6901257186f048969681c
Markinatorina <markinatorina@hotmail.com>
2026-06-13 07:38:36 +0200

    Split configuration runtime policy responses

--- MDRAVA.API/Controllers/ProxyConfigurationResponse.cs
+++ MDRAVA.API/Controllers/ProxyConfigurationResponse.cs
@@ -1,10 +1,6 @@
 using BusinessProxyConfigurationProjection =
     MDRAVA.BLL.ControlPlane.ConfigurationManagement.ProxyConfigurationProjection;
 using BusinessRuntimeAdminSecurityProjection = MDRAVA.BLL.Configuration.RuntimeAdminSecurityProjection;
-using BusinessRuntimeConnectionLimits = MDRAVA.BLL.Configuration.RuntimeConnectionLimits;
-using BusinessRuntimeForwardedHeadersOptions = MDRAVA.BLL.Configuration.RuntimeForwardedHeadersOptions;
-using BusinessRuntimeLimits = MDRAVA.BLL.Configuration.RuntimeLimits;
-using BusinessRuntimeTimeouts = MDRAVA.BLL.Configuration.RuntimeTimeouts;
 
 namespace MDRAVA.API.Controllers;
 
@@ -77,93 +73,5 @@ public sealed record RuntimeAdminSecurityResponse(
             projection.TokenEnvironmentVariable,
             projection.TokenSource,
             projection.RecentAuditCapacity);
-    }
-}
-
-public sealed record RuntimeTimeoutsResponse(
-    TimeSpan ClientRequestHeadTimeout,
-    TimeSpan ClientRequestBodyIdleTimeout,
-    TimeSpan UpstreamConnectTimeout,
-    TimeSpan UpstreamResponseHeadTimeout,
-    TimeSpan UpstreamResponseBodyIdleTimeout,
-    TimeSpan DownstreamWriteTimeout,
-    TimeSpan TlsHandshakeTimeout,
-    TimeSpan ClientKeepAliveIdleTimeout,
-    TimeSpan UpstreamIdleConnectionLifetime,
-    TimeSpan TunnelIdleTimeout)
-{
-    public static RuntimeTimeoutsResponse FromTimeouts(BusinessRuntimeTimeouts timeouts)
-    {
-        ArgumentNullException.ThrowIfNull(timeouts);
-
-        return new RuntimeTimeoutsResponse(
-            timeouts.ClientRequestHeadTimeout,
-            timeouts.ClientRequestBodyIdleTimeout,
-            timeouts.UpstreamConnectTimeout,
-            timeouts.UpstreamResponseHeadTimeout,
-            timeouts.UpstreamResponseBodyIdleTimeout,
-            timeouts.DownstreamWriteTimeout,
-            timeouts.TlsHandshakeTimeout,
-            timeouts.ClientKeepAliveIdleTimeout,
-            timeouts.UpstreamIdleConnectionLifetime,
-            timeouts.TunnelIdleTimeout);
-    }
-}
-
-public sealed record RuntimeConnectionLimitsResponse(
-    int MaxRequestsPerClientConnection,
-    int MaxIdleUpstreamConnectionsPerUpstream,
-    int MaxActiveUpgradedTunnels)
-{
-    public static RuntimeConnectionLimitsResponse FromLimits(BusinessRuntimeConnectionLimits limits)
-    {
-        ArgumentNullException.ThrowIfNull(limits);
-
-        return new RuntimeConnectionLimitsResponse(
-            limits.MaxRequestsPerClientConnection,
-            limits.MaxIdleUpstreamConnectionsPerUpstream,
-            limits.MaxActiveUpgradedTunnels);
-    }
-}
-
-public sealed record RuntimeLimitsResponse(
-    int MaxActiveClientConnections,
-    int MaxConcurrentTlsHandshakes,
-    int RequestsPerMinutePerIp,
-    int UpgradeRequestsPerMinutePerIp,
-    int MaxRequestHeadBytes,
-    int MaxHeaderCount,
-    int MaxHeaderLineBytes,
-    long MaxRequestBodyBytes,
-    int MaxPathBytes,
-    TimeSpan ShutdownGracePeriod)
-{
-    public static RuntimeLimitsResponse FromLimits(BusinessRuntimeLimits limits)
-    {
-        ArgumentNullException.ThrowIfNull(limits);
-
-        return new RuntimeLimitsResponse(
-            limits.MaxActiveClientConnections,
-            limits.MaxConcurrentTlsHandshakes,
-            limits.RequestsPerMinutePerIp,
-            limits.UpgradeRequestsPerMinutePerIp,
-            limits.MaxRequestHeadBytes,
-            limits.MaxHeaderCount,
-            limits.MaxHeaderLineBytes,
-            limits.MaxRequestBodyBytes,
-            limits.MaxPathBytes,
-            limits.ShutdownGracePeriod);
-    }
-}
-
-public sealed record RuntimeForwardedHeadersResponse(
-    bool Enabled,
-    IReadOnlyList<string> TrustedProxies)
-{
-    public static RuntimeForwardedHeadersResponse FromOptions(BusinessRuntimeForwardedHeadersOptions options)
-    {
-        ArgumentNullException.ThrowIfNull(options);
-
-        return new RuntimeForwardedHeadersResponse(options.Enabled, options.TrustedProxies.ToArray());
     }
 }
