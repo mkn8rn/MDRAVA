6c33e0d9cd92146fdcb1e0c45ff6005d0a8b6fed
Markinatorina <markinatorina@hotmail.com>
2026-06-13 13:31:11 +0200

    Split proxy metrics tunnel sinks

--- MDRAVA.BLL/ControlPlane/Metrics/ProxyMetricsDataPlaneSinks.cs
+++ MDRAVA.BLL/ControlPlane/Metrics/ProxyMetricsDataPlaneSinks.cs
@@ -168,47 +168,6 @@ public sealed partial class ProxyMetrics
 
     public void UpgradeUpstreamFailed() => Interlocked.Increment(ref _upgradeUpstreamFailures);
 
-    public ProxyTunnelAdmissionDecision StartTunnel(int maxActiveTunnels)
-    {
-        while (true)
-        {
-            var observed = Interlocked.Read(ref _activeTunnels);
-            if (observed >= maxActiveTunnels)
-            {
-                return ProxyTunnelAdmissionDecision.Rejected;
-            }
-
-            if (Interlocked.CompareExchange(ref _activeTunnels, observed + 1, observed) == observed)
-            {
-                return ProxyTunnelAdmissionDecision.Accepted;
-            }
-        }
-    }
-
-    public void TunnelStarted() => Interlocked.Increment(ref _totalTunnels);
-
-    public void TunnelClosed() => Interlocked.Decrement(ref _activeTunnels);
-
-    public void TunnelIdleTimedOut() => Interlocked.Increment(ref _tunnelIdleTimeouts);
-
-    public void AddTunnelBytesClientToUpstream(long bytes)
-    {
-        if (bytes > 0)
-        {
-            Interlocked.Add(ref _tunnelBytesClientToUpstream, bytes);
-        }
-    }
-
-    public void AddTunnelBytesUpstreamToClient(long bytes)
-    {
-        if (bytes > 0)
-        {
-            Interlocked.Add(ref _tunnelBytesUpstreamToClient, bytes);
-        }
-    }
-
-    public void TunnelRelayFailed() => Interlocked.Increment(ref _tunnelRelayFailures);
-
     public void UpstreamSelected(ProxyUpstreamSelectionMetric selection)
     {
         Interlocked.Increment(ref _upstreamSelections);
